5f3bf99c1ecab0f589900db5cdee76d819f70c8a
Yutaka Fujii <yutakaf@playwell.site>
2021-07-09 20:09:03 +0900

    余分なもの消す+Sassインストール

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const path = require('path')
+
+console.log(path.join(__dirname, 'styles'))
+
 module.exports = {
   reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
 }
